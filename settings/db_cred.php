5176ea37f91a94627fb49fcff6d32a5575705e19
Owusu-Banahene Osei <banaheneosei87@gmail.com>
2020-02-24 21:57:33 +0000

    Add db credentials to php.

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 //Database credentials
-define("DATABASE", "ScrapeTest");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+define("DATABASE", "u791583320_scrape_it");
+define("SERVER", "153.92.6.148");
+define("USERNAME", "u791583320_scrape_it");
+define("PASSWD", "password1234567890");
 
 ?>
