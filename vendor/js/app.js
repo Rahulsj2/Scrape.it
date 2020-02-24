40cf238fe35944662540953ddf87014c94885c0e
Rahulsj2 <rahul.srinivas@ashesi.edu.gh>
2020-02-24 14:56:26 +0000

    Frontend changes

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -24,3 +24,14 @@ $(document).ready(function () {
     $('#dtMaterialDesignExample_wrapper .mdb-select').materialSelect();
     $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('label').remove();
   });
+
+
+  // var url = document.location.toString();
+  // if (url.match('#')) {
+  //     $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+  // } 
+  
+  // // Change hash for page-reload
+  // $('.nav-tabs a').on('shown.bs.tab', function (e) {
+  //     window.location.hash = e.target.hash;
+  // })
