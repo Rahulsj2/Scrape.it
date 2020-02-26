36fe5fd7b36a144e7d7c48f61a023bd11f3da56b
Owusu-Banahene Osei <banaheneosei87@gmail.com>
2020-02-26 22:21:01 +0000

    Bug fixes for beauty scraper

--- Scrape/scraping/scrapylearn/spiders/beauty_spider.py
+++ Scrape/scraping/scrapylearn/spiders/beauty_spider.py
@@ -1,13 +1,5 @@
 import scrapy
 
-<<<<<<< HEAD
-class PostSpider(scrapy.Spider):
-    name = "beauty_spider"
-    # autothrottle_enabled = True
-    start_urls = ["https://www.theverge.com/tech"]
-=======
->>>>>>> c15208ffb324deb761b34da5b3d741da46078020
-
 class PostsBeautySpider(scrapy.Spider):
 
     name = "beauty_spider"
@@ -20,16 +12,17 @@ class PostsBeautySpider(scrapy.Spider):
         for post in response.css('div.full-item'):
             yield {
                 'title': post.css('div.full-item-content a.full-item-title::text').get(),
-                'post_link': post.css('div.full-item-content a.full-item-title::attr(href).getall()').getall(),
-                'image_link': post.css('a.full-time-image img.lazyimage::attr(src)'),
-                'summary': post.css('div.full-item-content div.full-item-dek p::text').getall(),
-                'date': post.css('div.full-item-metadata div.publish-date::attr(data-publish-date)').getall(),
-                'author': post.css('div.full-item-content div.byline span.byline-name::text').getall() #this returns the data with \n\t
+                'post_link': post.css('div.full-item-content a.full-item-title::attr(href)').get(),
+                'image_link': 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/screen-shot-2020-01-16-at-10-42-33-am-1579189394.png?crop=1.00xw:0.401xh;0,0&resize=768:*', #post.css('a.full-item-image img.lazyimage::attr(src)').get(),
+                'summary': post.css('div.full-item-content div.full-item-dek p::text').get(),
+                'date': post.css('div.full-item-metadata div.publish-date::attr(data-publish-date)').get(),
+                'author': post.css('div.full-item-content div.byline span.byline-name::text').get(), #this returns the data with \n\t
+                'category': 'beauty'
                 #Remember to trim this part of the code
             }
 
         
-        next_page = response.css('a.next-posts-link::attr(href)').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # next_page = response.css('a.next-posts-link::attr(href)').get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
